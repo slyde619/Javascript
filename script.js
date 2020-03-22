0ddcab422484c9096febbfd9fddd2241837cfbc0
slyde619 <slyde619@gmail.com>
2020-03-22 15:33:17 +0000

    My JS task

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let name = "Godwin Jonathan";
+let courses = ["HTML","CSS","JAVASCRIPT"];
+
+//Display name and courses offered
+console.log("My name is ", name, "and my courses offered are ",courses);
+
+for(let i = 0;i<=200;i++){
+    if (i % 2 !== 0){
+        console.log(i);
+    };
+}
